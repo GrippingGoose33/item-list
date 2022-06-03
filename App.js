7d482e8f018e499f8b2238b56263b04c8a3de480
GrippingGoose33 <grippinggoose33@hotmail.com>
2022-06-03 09:55:12 -0500

    first

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { Text, View, FlatList } from 'react-native';
 
 const data = new Array(100).fill(null).map((v, i) => ({key: i.toString(), value: `Item ${i}`}));
+
+function Item(props) {
+  return(
+    <Text style={styles.item}>{props.value}</Text>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
       <FlatList
         data = {data}
-        renderItem={(item) => <Text style={styles.item}>{item.value}</Text>}
+        renderItem={(item) => <Item value={item.value}/>}
       />
     </View>
   );
